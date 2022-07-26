89727ce6cda142432623f1b7315e416639cb39ef
lopo <lopo@zju.edu.cn>
2022-07-26 11:17:34 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,6 @@
+### study three.js
+
+#### links
+
+[three.js入门教程](http://ourjs.com/wiki/view/three)
+[WebGL中文网](http://www.hewebgl.com/article/articledir/1)
---
+++ vite.config.ts
@@ -0,0 +1,37 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { resolve } from "path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    base: './',
+    build: {
+        outDir: './dist',
+        rollupOptions: {
+            // output: {
+            //     sourcemap: 'hidden'
+            // },
+            manualChunks: (id) => {
+                if(id.includes('node_modules')) {
+                    return (id.toString().split('node_modules/')[1].split('/')[0])
+                }
+            }
+        }
+    },
+    plugins: [ vue() ],
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        }
+    },
+    server: {
+        port: 30789,
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                charset: false
+            }
+        }
+    }
+})
